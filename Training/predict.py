d1c759a3c9b323242fc7dd084fad4f7538654e55
Ubuntu <ubuntu@ip-172-31-61-99.us-west-2.compute.internal>
2020-01-30 22:51:58 +0000

    Added distributed learning functionality
    Modified get_soft_story_images.py and get_random_addresses.py so they can be called as functions or scripts.
    
    Added train.sh which trains the model across multiple ec2 instances using the horovod package.
    
    Modified train_inception_model.py and create_inception_model.py to incorporate horovod.

--- Training/predict.py
+++ Training/predict.py
@@ -2,11 +2,11 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
-image_filename = '/home/ubuntu/Insight/s3mnt/data2/preprocessed/non_soft_story/87.jpg'
+image_filename = '/home/ubuntu/Insight/s3mnt/data2/preprocessed/non_soft_story/76.jpg'
 model_filename = 'inception_model.h5'
 
 test_image = np.expand_dims(image.img_to_array(image.load_img(image_filename)), axis=0)
 model = load_model(model_filename)
 
 result = model.predict(test_image)
-print(result)
+print(np.argmax(result))
